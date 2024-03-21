0a128c444e3f85bb46e3fda8b4af18e58c50be39
Loic Lemaitre <loic.lemaitre@gmail.com>
2024-03-21 12:39:45 +0100

    Add Dape.

--- init.el
+++ init.el
@@ -316,6 +316,41 @@
   (setq realgud-safe-mode nil))
 
 ;;--------------------------------------------------------------------------------------------------
+;; DAPE
+;;--------------------------------------------------------------------------------------------------
+(use-package dape
+  :ensure t
+  ;; To use window configuration like gud (gdb-mi)
+  ;; :init
+  ;; (setq dape-buffer-window-arrangment 'gud)
+  :config
+  ;; Info buffers to the right
+  ;; (setq dape-buffer-window-arrangment 'right)
+
+  ;; To not display info and/or buffers on startup
+  ;; (remove-hook 'dape-on-start-hooks 'dape-info)
+  ;; (remove-hook 'dape-on-start-hooks 'dape-repl)
+
+  ;; To display info and/or repl buffers on stopped
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-info)
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-repl)
+
+  ;; By default dape uses gdb keybinding prefix
+  (setq dape-key-prefix "\C-x\C-a")
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-compile-hooks 'kill-buffer)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-on-start-hooks
+  ;;           (defun dape--save-on-start ()
+  ;;             (save-some-buffers t t)))
+
+  ;; Projectile users
+  (setq dape-cwd-fn 'projectile-project-root)
+)
+
+;;--------------------------------------------------------------------------------------------------
 ;; PO-MODE
 ;;--------------------------------------------------------------------------------------------------
 ;; Install gettext
